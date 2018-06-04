719e379f24b52935714971ca4f5948d65f77522a
Lu <luacosta.fc@gmail.com>
2018-06-04 18:43:37 -0500

    Creating actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,23 @@ class ActorsController < ApplicationController
     @actors = Actor.all
   end
  
+  def create
+    @actor = Actor.new(actor_params)
+    @actor.save
+    if @actor.save
+      redirect_to actors_path
+    else
+      render :new
+    end
+  end
+  
+  def new
+    @actor= Actor.new
+  end
+
+  private
+
+  def actor
+    params.require(:actor).permit(:image_url, :bio, :birth_date, :birth_place, :alive, :death_date, :death_place)
+  end
 end
