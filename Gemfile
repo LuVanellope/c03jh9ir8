fcfc7aba38c9bd6f731ab1a437cfe373da4fc739
Lu <luacosta.fc@gmail.com>
2018-09-25 16:36:45 -0500

    adding bootstrap and labels in new actor

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '5.0.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
   # Use SCSS for stylesheets      # Use SCSS for stylesheets
+gem 'bootstrap', '~> 4.0.0'
 gem 'sass-rails', '~> 5.0'
   # Use Uglifier as compressor for JavaScript assets      # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,17 +2,21 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
- 
+
   def create
-    @actor = Actor.new(actor_params)
-    @actor.save
-    if @actor.save
-      redirect_to actors_path
+    actor_params = params.require(:actor).permit!
+    if actor_params["death_date"].present? && actor_params["death_place"].present?
+      actor_params.merge!({"alive" => false})
     else
-      render :new
+      actor_params.merge!({"alive" => true})
+    end
+    actor = Actor.new(actor_params)
+
+    if actor.save
+      redirect_to actors_path
     end
   end
-  
+
   def new
     @actor= Actor.new
   end
